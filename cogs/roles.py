7718d4e3ac9e2a97317346766bfb931a3f60e8f1
Saber_0324 <saber0324@proton.me>
2026-05-24 17:38:09 -0400

    Added add and delete subcommands to role.

--- cogs/roles.py
+++ cogs/roles.py
@@ -33,18 +33,47 @@ class Roles(commands.Cog):
         else:
             await interaction.response.send_message("Role not found.")
 
-    @role_group.command(name="list")
+    @role_group.command(
+        name="list", description="Shows a list all of the server's roles."
+    )
     async def role_list(self, interaction: discord.Interaction) -> None:
         logger.info(f"{interaction.user} requested list of roles")
         await interaction.response.send_message(
             embed=await embeds.roleListEmbed(interaction)
         )
 
-    @role_group.command()
-    async def count(
-        self, interaction: discord.Interaction, *, group: str | None = None
+    @role_group.command(description="Adds a role to an user.")
+    @app_commands.describe(
+        user="User that will get a role added.", role="Role that will be added to user."
+    )
+    async def add(
+        self, interaction: discord.Interaction, user: discord.Member, role: discord.Role
     ) -> None:
-        pass
+        if interaction.permissions.manage_roles:
+            logging.info(f"{interaction.user} has added role {role} to {user}")
+            await user.add_roles(role)
+            await interaction.response.send_message(
+                f"The role {role.name} has been added to {user.name}."
+            )
+        else:
+            raise app_commands.MissingPermissions(["manage_roles"])
+
+    @role_group.command(description="Removes a role from an user.")
+    @app_commands.describe(
+        user="User that will get a role removed.",
+        role="Role that will be removed from user.",
+    )
+    async def remove(
+        self, interaction: discord.Interaction, user: discord.Member, role: discord.Role
+    ) -> None:
+        if interaction.permissions.manage_roles:
+            logging.info(f"{interaction.user} has removed role {role} to {user}")
+            await user.remove_roles(role)
+            await interaction.response.send_message(
+                f"The role {role.name} has been removed from {user.name}."
+            )
+        else:
+            raise app_commands.MissingPermissions(["manage_roles"])
 
 
 async def setup(bot: Hux) -> None:
