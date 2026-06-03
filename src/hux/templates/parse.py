348d9d4349e75e57e568e012b807e56732697f3d
Saber_0324 <saber0324@proton.me>
2026-06-03 17:52:55 -0400

    Added a check for codeblock escaping in extract code

--- src/hux/templates/parse.py
+++ src/hux/templates/parse.py
@@ -6,5 +6,7 @@ CODE_PATTERN = r"```+[ \t]*(.*?)[ \t]*\n(.*?)```+"
 def extract_code(pattern: str, text: str) -> tuple[str, str]:
     match = re.search(pattern, text, re.DOTALL | re.IGNORECASE)
     if match:
+        if "`" in match.group(2):
+            return match.group(1), "tried codeblock escaping"
         return match.group(1), match.group(2)
     return "Language unmatched", "No code"
