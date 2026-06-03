bbcb25c1d99403d2503794d60adcbe3875ed5c4a
Saber_0324 <saber0324@proton.me>
2026-06-03 17:44:46 -0400

    Deleted reply test

--- src/hux/cogs/testing.py
+++ src/hux/cogs/testing.py
@@ -28,38 +28,6 @@ class Testing(commands.Cog):
         logger.info(f"Left the server {guild.name} with id {guild.id}")
         await guild.leave()
 
-    @commands.command(name="replied")
-    async def reply(self, ctx: commands.Context, *, text: str) -> None:
-        reply_message = await ctx.reply("Fake eval result\n```hello world```")
-        self.eval_message_pairs.append((reply_message, ctx.message))
-        logger.info(f"User msg: {ctx.message.id}, bot msg: {reply_message.id}")
-
-    @commands.Cog.listener()
-    async def on_message_edit(
-        self, before: discord.Message, after: discord.Message
-    ) -> None:
-        for _, user_message in self.eval_message_pairs:
-            if user_message.id == after.id and before.edited_at is None:
-                await after.add_reaction("\U0001f501")
-
-    @commands.Cog.listener()
-    async def on_reaction_add(
-        self, reaction: discord.Reaction, user: discord.User | discord.Member
-    ) -> None:
-        if user.bot:
-            return
-
-        for bot_reply, user_message in reversed(self.eval_message_pairs):
-            if reaction.emoji == "\U0001f501" and user.id == user_message.author.id:
-                await bot_reply.reply(
-                    f"reacted message is: {user_message.jump_url}\n"
-                    f"bot's message is: {bot_reply.jump_url}"
-                )
-                await reaction.message.clear_reactions()
-                self.eval_message_pairs.remove((bot_reply, user_message))
-
-                break
-
 
 async def setup(bot: Hux):
     await bot.add_cog(Testing(bot))
